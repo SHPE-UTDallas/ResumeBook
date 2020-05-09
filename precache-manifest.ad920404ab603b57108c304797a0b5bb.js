7838f0e4fe9dfc5c9e664740ebd0d8efc58df1ad
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-05-08 22:09:15 -0500

    Updates

---
+++ precache-manifest.ad920404ab603b57108c304797a0b5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de79c4a53731fa4a3c0e6b91923d7c35",
+    "url": "/ResumeBook/index.html"
+  },
+  {
+    "revision": "78af231a2a9afd363e74",
+    "url": "/ResumeBook/static/css/main.f1896a18.chunk.css"
+  },
+  {
+    "revision": "70b5cfaf38ec661ea26e",
+    "url": "/ResumeBook/static/js/2.d3d16e31.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ResumeBook/static/js/2.d3d16e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78af231a2a9afd363e74",
+    "url": "/ResumeBook/static/js/main.7354be13.chunk.js"
+  },
+  {
+    "revision": "56d0934d2b69a6742331",
+    "url": "/ResumeBook/static/js/runtime-main.81aebea0.js"
+  }
+]);
