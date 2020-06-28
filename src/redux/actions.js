4d5dfda4bb437cead32cffc8cf0a29e1aa721f92
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-06-28 01:59:44 -0500

    Setup the backend for users to authenticate themselves as officers. Setup redux to store a user's officer status

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -75,10 +75,11 @@ export const toggleDrawer = () => ({
   type: TOGGLE_DRAWER,
 })
 
-export const loginSuccess = (verified) => ({
+export const loginSuccess = (verified, officer) => ({
   type: LOGIN_SUCCESS,
   payload: {
     verified,
+    officer,
   },
 })
 
--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -3,17 +3,20 @@ import { LOGIN_SUCCESS, LOGOUT_SUCCESS } from '../actionTypes'
 const initialState = {
   isAuthenticated: false,
   isVerified: false,
+  isOfficer: false,
 }
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case LOGIN_SUCCESS: {
-      let { verified } = action.payload
+      let { verified, officer } = action.payload
       verified = verified === true ? true : false
+      officer = officer === true ? true : false
       return {
         ...state,
         isAuthenticated: true,
         isVerified: verified,
+        isOfficer: officer,
       }
     }
     case LOGOUT_SUCCESS: {
