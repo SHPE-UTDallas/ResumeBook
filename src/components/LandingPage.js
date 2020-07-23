22cc3c26803ad4694c37f1c5c046bd909f974187
David Gutierrez <david.a.gutierrez99@gmail.com>
2020-07-23 00:48:32 -0500

    Added some text and stlyed buttons

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,18 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from '@material-ui/core'
 import { Link as RouterLink } from 'react-router-dom'
+import './styles/LandingPage.sass'
 
 function App(props) {
   const classes = props.classes
   return (
     <div className={classes.content}>
       <div className={classes.toolbar} />
-      <Button variant="contained" color="primary" to="/demo" component={RouterLink}>
-        Demo
-      </Button>
-      <Button variant="contained" color="primary" to="/resumes" component={RouterLink}>
-        Sponsor
-      </Button>
+      <div id="info">
+        <h1 id="info-title">What is ResumeBook?</h1>
+        <div id="info-content">
+          <p>
+            ResumeBook was created by the members of SHPE UTD to bring sponsors a simple
+            and unique way to find the resumes they need. It gives the ability to search
+            by GPA, major, and even year. To see experience a preview of what this might
+            look like, go ahead and try the demo! If you are a sponsor of SHPE UTD, go
+            ahead and log in through the sponsor button.
+          </p>
+          <div id="demo-sponsor">
+            <Button variant="contained" color="primary" to="/demo" component={RouterLink}>
+              Demo
+            </Button>
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              to="/resumes"
+              component={RouterLink}
+            >
+              Sponsor
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
