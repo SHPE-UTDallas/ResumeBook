994ace0102a3f557f2b65d1b94993988a2ed126d
David Gutierrez <35023517+davidagtz@users.noreply.github.com>
2020-08-22 15:27:37 -0500

    Landing page (#38)
    * Added some text and stlyed buttons
    
    * made buttons mobile responsive
    
    * Moved to grid for more responsive page
    
    * wireframe carousel
    
    * working prototype
    
    * Added timer and responsive width
    
    * Fixed animations
    
    * Fix key prop error and minor code cleanup
    
    * Fixed previous slide not working after the code cleanup. Did more refactoring
    
    * Fixed bug when clicking next slide on the last image in the carousel
    
    * Made the height responsive with aspect prop
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/LandingInfo.js
    
    Co-authored-by: Gabriel <gm.flynn99@yahoo.com>
    
    * Update src/components/styles/LandingPage.sass
    
    Co-authored-by: Gabriel Flynn <gm.flynn99@yahoo.com>
    
    * Got rid of dependency
    
    Co-authored-by: Gabriel Flynn <gm.flynn99@yahoo.com>

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,206 @@
+import React from 'react'
+import './styles/Carousel.sass'
+
+interface CarouselProps {
+  width: string
+  maxWidth: string
+  height: string
+  className: string
+  auto?: boolean
+  aspect?: [number, number]
+  children: React.ReactNode
+}
+
+export default class extends React.Component<
+  CarouselProps,
+  {
+    activeOn: number
+  }
+> {
+  state = {
+    activeOn: 0,
+  }
+  forward: boolean = false
+  length: number
+  outerLength: number
+  css: string
+  auto: boolean = false
+  interval: NodeJS.Timeout | null = null
+  initial: boolean = true
+  wrapper: React.RefObject<HTMLDivElement>
+
+  constructor(props: CarouselProps) {
+    super(props)
+
+    this.wrapper = React.createRef()
+
+    this.length = React.Children.count(props.children)
+    this.outerLength = this.length + 2
+    this.prevSlide = this.prevSlide.bind(this)
+    this.nextSlide = this.nextSlide.bind(this)
+    this.nextPress = this.nextPress.bind(this)
+    this.prevPress = this.prevPress.bind(this)
+    this.resize = this.resize.bind(this)
+
+    if (props.auto) {
+      this.auto = true
+    }
+
+    this.css = ''
+    const imgW = 100 / this.outerLength
+
+    this.css += `@keyframes init { 0% {transform: translate(-${imgW}%, 0)} 100% {transform: translate(-${imgW}%, 0)}}\n`
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0) {
+        this.css += `@keyframes nextSlide-0 {`
+        this.css += `0% { transform: translate(0, 0)}`
+        this.css += `100% { transform: translate(-${imgW * (i + 1)}%, 0)}}\n`
+      } else {
+        this.css += `@keyframes nextSlide-${i} {`
+        this.css += `0% { transform: translate(-${imgW * i}%, 0)}`
+        this.css += `100% { transform: translate(-${imgW * (i + 1)}%, 0)}}\n`
+      }
+
+      if (i === this.length - 1) {
+        this.css += `@keyframes prevSlide-${i} {`
+        this.css += `0% { transform: translate(-${imgW * (this.outerLength - 1)}%, 0)}`
+        this.css += `100% { transform: translate(-${imgW * (i + 1)}%, 0)}}\n`
+      } else {
+        this.css += `@keyframes prevSlide-${i} {`
+        this.css += `0% { transform: translate(-${imgW * (i + 2)}%, 0)}`
+        this.css += `100% { transform: translate(-${imgW * (i + 1)}%, 0)}}\n`
+      }
+    }
+  }
+
+  makeCarouselItem(child: any, key: any) {
+    return (
+      <div
+        className="carousel-item"
+        key={key}
+        style={{ width: `${100 / this.length}%`, maxHeight: this.props.height }}
+      >
+        {child}
+      </div>
+    )
+  }
+
+  render() {
+    const { children, width, height, maxWidth } = this.props
+    const buttonWidth = '4em'
+    const windowWidth = `calc(100% - 2 * ${buttonWidth})`
+
+    const usableChildren = React.Children.toArray(children)
+
+    const slides = usableChildren.map((e, i) => this.makeCarouselItem(e, `ci-${i}`))
+
+    slides.unshift(
+      this.makeCarouselItem(usableChildren[usableChildren.length - 1], 'ci-beg')
+    )
+    slides.push(this.makeCarouselItem(usableChildren[0], 'ci-end'))
+
+    if (slides.length === 0) throw new Error('Carousel must have at least one child')
+
+    const indicators: JSX.Element[] = []
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.state.activeOn) {
+        indicators.push(<li key={`c-li-${i}`} className="active" />)
+      } else {
+        indicators.push(<li key={`c-li-${i}`} />)
+      }
+    }
+
+    const anim = this.forward
+      ? `nextSlide-${this.state.activeOn}`
+      : `prevSlide-${this.state.activeOn}`
+
+    return (
+      <div
+        className="carousel"
+        style={{
+          width,
+          height,
+          maxWidth,
+        }}
+        ref={this.wrapper}
+      >
+        <style>{this.css}</style>
+        <div className="top">
+          <div className="button" onClick={this.prevPress}>
+            <div className="left" />
+          </div>
+          <div className="items" style={{ flexBasis: windowWidth }}>
+            <div
+              className="photo-window"
+              style={{
+                width: `${100 * this.outerLength}%`,
+                animationName: this.initial ? 'init' : anim,
+              }}
+            >
+              {slides}
+            </div>
+          </div>
+          <div className="button" onClick={this.nextPress}>
+            <div className="right" />
+          </div>
+        </div>
+        <ul className="indicators">{indicators}</ul>
+      </div>
+    )
+  }
+
+  resize() {
+    if (this.props.aspect) {
+      const { aspect } = this.props
+      const carousel = this.wrapper.current!
+      const width = carousel.getBoundingClientRect().width
+      carousel.style.height = `calc(${aspect[1] / aspect[0]} * ${width}px)`
+    }
+  }
+
+  componentDidMount() {
+    if (this.auto) {
+      if (this.interval) {
+        clearInterval(this.interval)
+      }
+      this.interval = setInterval(this.nextSlide, 3000)
+    }
+    this.initial = false
+
+    window.addEventListener('resize', this.resize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+
+  nextPress() {
+    this.componentDidMount()
+    this.nextSlide()
+  }
+
+  prevPress() {
+    this.componentDidMount()
+    this.prevSlide()
+  }
+
+  nextSlide() {
+    this.forward = true
+    this.setState({
+      activeOn: (this.state.activeOn + 1) % this.length,
+    })
+  }
+
+  prevSlide() {
+    this.forward = false
+    if (this.state.activeOn !== 0) {
+      this.setState({
+        activeOn: (this.state.activeOn - 1) % this.length,
+      })
+    } else {
+      this.setState({
+        activeOn: this.length - 1,
+      })
+    }
+  }
+}
---
+++ src/components/LandingInfo.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Grid, Button } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
+
+export default class LandingInfo extends React.Component {
+  render() {
+    return (
+      <div id="info">
+        <h1 id="info-title">What is ResumeBook?</h1>
+        <Grid container id="info-content">
+          <Grid item xs={12} sm={8}>
+            <p>
+              ResumeBook was created by the members of SHPE UTD to bring sponsors a simple
+              and unique way to find the resumes they need. It gives the ability to search
+              by GPA, major, and even year. To see a preview of what this might look like,
+              go ahead and try the demo! If you are a sponsor of SHPE UTD, go ahead and
+              log in through the sponsor button.
+            </p>
+          </Grid>
+          <Grid item xs={2} sm={1} />
+          <Grid item container direction="column" xs={8} sm={3} id="demo-sponsor">
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                to="/demo"
+                component={RouterLink}
+              >
+                Demo
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                to="/resumes"
+                component={RouterLink}
+              >
+                Sponsor
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    )
+  }
+}
