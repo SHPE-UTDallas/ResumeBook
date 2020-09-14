9149d63159918ab28971f6771949bf564e1507f4
Rigre R. Garciandia <rigrergl@gmail.com>
2020-09-14 15:14:11 -0500

    Added error message to the verify page

--- src/routes/verify/index.js
+++ src/routes/verify/index.js
@@ -7,11 +7,12 @@ import { connect } from 'react-redux'
 import { loginSuccess } from '../../redux/actions'
 import { withRouter } from 'react-router-dom'
 import { ENDPOINT } from '../../utils/config'
+import Alert from '@material-ui/lab/Alert'
 
 class VerifyForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { value: '' }
+    this.state = { value: '', errorMessage: '' }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -33,8 +34,14 @@ class VerifyForm extends React.Component {
       localStorage.setItem('verified', true)
       this.props.loginSuccess(true)
       this.props.history.push('/resumes')
+
+      this.setState({
+        errorMessage: '',
+      })
     } else {
-      //TODO: Add error messages
+      this.setState({
+        errorMessage: response,
+      })
     }
   }
 
@@ -47,6 +54,11 @@ class VerifyForm extends React.Component {
           <div className={classes.toolbar} />
           <Grid container alignItems="center" direction="column" justify="center">
             <h3>Please verify you account by inputting the code provided to you</h3>
+            {this.state.errorMessage === '' ? (
+              <React.Fragment />
+            ) : (
+              <Alert severity="error">{this.state.errorMessage}</Alert>
+            )}
             <form onSubmit={this.handleSubmit}>
               <FormControlLabel
                 className={classes.formEntry}
