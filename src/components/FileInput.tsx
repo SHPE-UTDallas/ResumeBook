4716a7fae8637a3def5dcbd6df5107046ab64e15
David Gutierrez <35023517+davidagtz@users.noreply.github.com>
2020-09-15 17:24:33 -0500

    Linked in urls (#47)
    * Made upload page nicer
    
    * Moved to material buttons
    
    * Added file validator and typing
    
    * Added major select with other option
    
    * Corrects linkedin urls
    
    * Made the linkedin compatible with usernames
    
    * Cleaned up code
    
    * Redirects to home after uploading
    
    * removed dependency and changed the order for cloudinary
    
    * Add disabling buttons for submission
    
    * Fixed regex and http case considered
    
    * removed print statement
    
    * Fix merge
    
    Co-authored-by: Gabriel Flynn <gm.flynn99@yahoo.com>

---
+++ src/components/FileInput.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import './styles/FileInput.sass'
+
+export default class FileInput extends React.Component<{
+  name: string
+  accept: string
+}> {
+  wrapper: React.Ref<HTMLInputElement>
+  state = {
+    file: '',
+  }
+
+  constructor(props: any) {
+    super(props)
+    this.wrapper = React.createRef()
+    this.selectFile = this.selectFile.bind(this)
+    this.changeFile = this.changeFile.bind(this)
+  }
+
+  changeFile() {
+    const input = (this.wrapper as any).current as HTMLInputElement
+    if (input.value !== '') {
+      const index = input.value.indexOf('fakepath') + 9
+      this.setState({ file: input.value.substring(index) })
+    }
+  }
+
+  selectFile() {
+    const input = (this.wrapper as any).current as HTMLInputElement
+    input.click()
+  }
+
+  render() {
+    return (
+      <div className="dg-file-input">
+        <input
+          type="file"
+          onChange={this.changeFile}
+          ref={this.wrapper}
+          accept={this.props.accept}
+          name={this.props.name}
+          hidden
+        />
+        <Button variant="contained" onClick={this.selectFile} color="primary">
+          Select File
+        </Button>
+        {this.state.file === '' ? null : <span>{this.state.file}</span>}
+      </div>
+    )
+  }
+}
