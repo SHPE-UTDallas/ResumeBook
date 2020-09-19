fd9db28bc5316c0650d95c67dc993a52f301028b
Leonardo Duenes <Leonardo7duenes@gmail.com>
2020-09-19 15:45:46 -0500

    Added 404

--- src/routes/404.js
+++ src/routes/404.js
@@ -1,10 +1,20 @@
-// import React from 'react';
+import React from 'react'
 
-// function App(props) {
-//   return (
-//     <div>
-//       <h1>404</h1>
-//     </div>
-//   );
-// }
-// export default App;
+function App() {
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1 style={{ margin: '21.44px 10px' }}>404</h1>
+        <img
+          style={{ width: '55.22px', height: '55.22px', margin: '21.44px 10px' }}
+          alt="SHPE logo"
+          src="https://i0.wp.com/www.shpesd.org/wp-content/uploads/2020/02/cropped-SHPE_logo_vert_SanDiegoPro_CMYK_HalfSize.png?fit=512%2C512"
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <a href="/">Home</a>
+      </div>
+    </>
+  )
+}
+export default App
