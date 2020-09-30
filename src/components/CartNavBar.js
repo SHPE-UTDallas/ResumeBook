a849c257ef3c9e286578155411c4c5fe372bdf77
Eddie Villarreal <70770340+ev168400@users.noreply.github.com>
2020-09-30 14:40:45 -0500

    Added dropdown for approval (#74)

--- src/components/CartNavBar.js
+++ src/components/CartNavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import PersonIcon from '@material-ui/icons/Person';
+import PersonIcon from '@material-ui/icons/Person'
 import Typography from '@material-ui/core/Typography'
 
 class Cart extends React.Component {
