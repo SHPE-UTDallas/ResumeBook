863596d0603285caad26d6a2929c0b1ad055d270
Eddie <ejv190000@utdallas.edu>
2020-09-30 14:37:39 -0500

    Added dropdown for approval

--- src/routes/officer/index.js
+++ src/routes/officer/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import NavBar from '../../components/NavBar'
 import { ENDPOINT } from '../../utils/config'
 import ApprovalTable from '../../components/ApprovalTable'
-import FilterPane from '../../components/FilterPane'
+import FilterPaneApproved from '../../components/FilterPaneApproved'
 
 function Officer(props) {
   const classes = props.classes
@@ -12,7 +12,7 @@ function Officer(props) {
   return (
     <div className={classes.root}>
       <NavBar enableDrawer={true} classes={classes} />
-      <FilterPane classes={classes} />
+      <FilterPaneApproved classes={classes} />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <h1 style={{ textAlign: 'center' }}>Members</h1>
