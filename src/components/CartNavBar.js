df9cabf6bfdf67c115e9debd04e46f822b909ed9
Eddie <ejv190000@utdallas.edu>
2020-10-14 14:30:09 -0500

    Added link functionality to person icon

--- src/components/CartNavBar.js
+++ src/components/CartNavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import PersonIcon from '@material-ui/icons/Person'
 import Typography from '@material-ui/core/Typography'
+import { Link } from 'react-router-dom'
 import './styles/NavBar.sass'
 
 class Cart extends React.Component {
@@ -13,12 +14,12 @@ class Cart extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <Link to="/cart">
         <Typography style={{ marginTop: '-8px' }} className={this.props.classes.cartText}>
           {this.props.numInCart}
         </Typography>
         <PersonIcon className={this.props.classes.cart} />
-      </React.Fragment>
+      </Link>
     )
   }
 }
