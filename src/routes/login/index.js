a4c64e82c8fe4f78ba56c7cf3549009ba04f2b6b
Mahir Morar <MahirMorar17@users.noreply.github.com>
2020-10-22 15:39:16 -0500

    Updated login page

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -3,18 +3,29 @@ import PropTypes from 'prop-types'
 import NavBar from '../../components/NavBar'
 import { LinkedInLoginButton } from 'react-social-login-buttons'
 import { ENDPOINT } from '../../utils/config'
+import { Grid } from '@material-ui/core'
 
 function App(props) {
   const classes = props.classes
   const endpoint_url = `${ENDPOINT}/auth/linkedin`
+
+  const mystyle = {
+    border: 0,
+    marginTop: 20,
+    textAlign: 'center',
+    padding: '10px',
+  }
   return (
     <div className={classes.root}>
       <NavBar enableDrawer={false} classes={classes} />
       <div className={classes.content}>
         <div className={classes.toolbar} />
-        <a href={endpoint_url}>
-          <LinkedInLoginButton />
-        </a>
+        <h1 style={mystyle}> Welcome Back. Please sign in with LinkedIn below </h1>
+        <Grid item md={3} sm={6} xs={10} style={{ margin: '0 auto' }}>
+          <a href={endpoint_url} style={{ textDecoration: 'none' }}>
+            <LinkedInLoginButton />
+          </a>
+        </Grid>
       </div>
     </div>
   )
